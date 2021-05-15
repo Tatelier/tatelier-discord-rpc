8f662b3f4313bc0ba96bc25e452a2b41c8f62edc
Pansystar <wh.bt3.p@gmail.com>
2021-05-15 23:41:44 +0900

    文字化け対応

--- TatelierDiscordRPC/RichPresence.cs
+++ TatelierDiscordRPC/RichPresence.cs
@@ -1,22 +1,45 @@
-﻿namespace Tatelier.Module.DiscordRPC
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Tatelier.Module.DiscordRPC
 {
-	[System.Serializable]
+	[Serializable, StructLayout(LayoutKind.Sequential)]
 	struct RichPresence
 	{
-		public string State; /* max 128 bytes */
-		public string Details; /* max 128 bytes */
+		public IntPtr State; /* max 128 bytes */
+		public IntPtr Details; /* max 128 bytes */
 		public long StartTimestamp;
 		public long EndTimestamp;
-		public string LargeImageKey; /* max 32 bytes */
-		public string LargeImageText; /* max 128 bytes */
-		public string SmallImageKey; /* max 32 bytes */
-		public string SmallImageText; /* max 128 bytes */
-		public string PartyId; /* max 128 bytes */
+		public IntPtr LargeImageKey; /* max 32 bytes */
+		public IntPtr LargeImageText; /* max 128 bytes */
+		public IntPtr SmallImageKey; /* max 32 bytes */
+		public IntPtr SmallImageText; /* max 128 bytes */
+		public IntPtr PartyId; /* max 128 bytes */
 		public int PartySize;
 		public int PartyMax;
-		public string MatchSecret; /* max 128 bytes */
-		public string JoinSecret; /* max 128 bytes */
-		public string SpectateSecret; /* max 128 bytes */
+		public IntPtr MatchSecret; /* max 128 bytes */
+		public IntPtr JoinSecret; /* max 128 bytes */
+		public IntPtr SpectateSecret; /* max 128 bytes */
 		public bool Instance;
-    }
+
+		public RichPresence(string a)
+		{
+			State = Marshal.AllocHGlobal(128);
+			Details = Marshal.AllocHGlobal(128);
+			StartTimestamp = 0;
+			EndTimestamp = 0;
+			LargeImageKey = Marshal.AllocHGlobal(32);
+			LargeImageText = Marshal.AllocHGlobal(128);
+			SmallImageKey = Marshal.AllocHGlobal(32);
+			SmallImageText = Marshal.AllocHGlobal(128);
+			PartyId = Marshal.AllocHGlobal(128);
+			PartySize = 0;
+			PartyMax = 0;
+			MatchSecret = Marshal.AllocHGlobal(128);
+			JoinSecret = Marshal.AllocHGlobal(128);
+			SpectateSecret = Marshal.AllocHGlobal(128);
+			Instance = false;
+		}
+	}
 }
--- TatelierDiscordRPC/RichPresenceWrapper.cs
+++ TatelierDiscordRPC/RichPresenceWrapper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,20 +11,61 @@ namespace Tatelier.Module.DiscordRPC
     {
         public RichPresence Data;
 
-        string IRichPresence.State { get => Data.State; set => Data.State = value; }
-        string IRichPresence.Details { get => Data.Details; set => Data.Details = value; }
+        byte[] bytes = new byte[128];
+
+        string GetText(IntPtr ptr)
+		{
+            Array.Clear(bytes, 0, bytes.Length);
+            Marshal.Copy(ptr, bytes, 0, bytes.Length);
+
+            string result = Encoding.UTF8.GetString(bytes);
+            int index = result.IndexOf('\0');
+            if (index >= 0) result = result.Substring(0, index);
+            return result;
+        }
+
+        void SetText(string text, ref IntPtr ptr)
+        {
+			if (ptr != IntPtr.Zero)
+			{
+                Marshal.FreeHGlobal(ptr);
+			}
+
+			if (string.IsNullOrEmpty(text))
+            {
+                ptr = IntPtr.Zero;
+                return;
+            }
+            var count = Encoding.UTF8.GetByteCount(text);
+            var buffer = Marshal.AllocHGlobal(count + 1);
+            for (int i = 0; i < count + 1; i++)
+            {
+                Marshal.WriteByte(buffer, i, 0);
+            }
+
+            Marshal.Copy(Encoding.UTF8.GetBytes(text), 0, buffer, count);
+            ptr = buffer;
+        }
+
+        string IRichPresence.State { get => GetText(Data.State); set => SetText(value, ref Data.State); }
+        string IRichPresence.Details { get => GetText(Data.Details); set => SetText(value, ref Data.Details); }
         long IRichPresence.StartTimestamp { get => Data.StartTimestamp; set => Data.StartTimestamp = value; }
         long IRichPresence.EndTimestamp { get => Data.EndTimestamp; set => Data.EndTimestamp = value; }
-        string IRichPresence.LargeImageKey { get => Data.LargeImageKey; set => Data.LargeImageKey = value; }
-        string IRichPresence.LargeImageText { get => Data.LargeImageText; set => Data.LargeImageText = value; }
-        string IRichPresence.SmallImageKey { get => Data.SmallImageKey; set => Data.SmallImageKey = value; }
-        string IRichPresence.SmallImageText { get => Data.SmallImageText; set => Data.SmallImageText = value; }
-        string IRichPresence.PartyId { get => Data.PartyId; set => Data.PartyId = value; }
+        string IRichPresence.LargeImageKey { get => GetText(Data.LargeImageKey); set => SetText(value, ref Data.LargeImageKey); }
+        string IRichPresence.LargeImageText { get => GetText(Data.LargeImageText); set => SetText(value, ref Data.LargeImageText); }
+        string IRichPresence.SmallImageKey { get => GetText(Data.SmallImageKey); set => SetText(value, ref Data.SmallImageKey); }
+        string IRichPresence.SmallImageText { get => GetText(Data.SmallImageText); set => SetText(value, ref Data.SmallImageText); }
+        string IRichPresence.PartyId { get => GetText(Data.PartyId); set => SetText(value, ref Data.State); }
         int IRichPresence.PartySize { get => Data.PartySize; set => Data.PartySize = value; }
         int IRichPresence.PartyMax { get => Data.PartyMax; set => Data.PartyMax = value; }
-        string IRichPresence.MatchSecret { get => Data.MatchSecret; set => Data.MatchSecret = value; }
-        string IRichPresence.JoinSecret { get => Data.JoinSecret; set => Data.JoinSecret = value; }
-        string IRichPresence.SpectateSecret { get => Data.SpectateSecret; set => Data.SpectateSecret = value; }
+        string IRichPresence.MatchSecret { get => GetText(Data.MatchSecret); set => SetText(value, ref Data.MatchSecret); }
+        string IRichPresence.JoinSecret { get => GetText(Data.JoinSecret); set => SetText(value, ref Data.JoinSecret); }
+        string IRichPresence.SpectateSecret { get => GetText(Data.SpectateSecret); set => SetText(value, ref Data.SpectateSecret); }
         bool IRichPresence.Instance { get => Data.Instance; set => Data.Instance = value; }
+
+        public RichPresenceWrapper()
+		{
+            Data = new RichPresence();
+		}
     }
 }
